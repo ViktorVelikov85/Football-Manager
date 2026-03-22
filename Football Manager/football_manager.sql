c7841abb585de6d9a05a4b65e752bf6f8eba98fd
Viktor <victorvelikov27@itpg-varna.bg>
2026-03-22 13:28:21 +0200

    Update football_manager.sql

--- Football Manager/football_manager.sql
+++ Football Manager/football_manager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 16, 2026 at 06:48 PM
+-- Generation Time: Mar 22, 2026 at 12:27 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -69,24 +69,22 @@ INSERT INTO `players` (`id`, `club_id`, `full_name`, `birth_date`, `position`, `
 (1, 1, 'Мартин Луков', '1993-07-05', 'GK', 78, 'Active'),
 (2, 1, 'Майкон', '1999-02-10', 'DF', 3, 'Active'),
 (3, 1, 'Кристиан Макун', '2000-03-05', 'DF', 4, 'Active'),
-(4, 1, 'Стипе Вуликич', '2001-01-01', 'DF', 6, 'Active'),
+(4, 3, 'Стипе Вуликич', '2001-01-01', 'DF', 6, 'Active'),
 (5, 1, 'Алдаир Невеш', '1999-05-10', 'DF', 21, 'Active'),
-(6, 1, 'Карлос Охене', '1992-07-21', 'MF', 8, 'Active'),
+(6, 3, 'Карлос Охене', '1992-07-21', 'MF', 8, 'Active'),
 (7, 1, 'Асен Митков', '2004-02-16', 'MF', 10, 'Active'),
-(8, 1, 'Патрик Мислович', '2001-05-28', 'MF', 23, 'Active'),
+(8, 1, 'Патрик Мислович', '2001-05-28', 'MF', 23, 'Injured'),
 (9, 1, 'Евертон Бала', '1999-01-03', 'MF', 17, 'Active'),
 (10, 1, 'Марин Петков', '2003-10-02', 'FW', 88, 'Active'),
 (11, 1, 'Мустафа Сангаре', '1998-12-12', 'FW', 12, 'Active'),
 (12, 2, 'Хендрик Бонман', '1994-01-19', 'GK', 1, 'Active'),
-(13, 2, 'Йоел Андерсон', '1996-11-11', 'DF', 2, 'Active'),
+(13, 3, 'Йоел Андерсон', '1996-11-11', 'DF', 2, 'Active'),
 (14, 2, 'Антон Недялков', '1993-04-30', 'DF', 3, 'Active'),
 (15, 2, 'Оливие Вердон', '1995-10-05', 'DF', 24, 'Active'),
 (16, 2, 'Диниш Алмейда', '1995-06-28', 'DF', 4, 'Active'),
 (17, 2, 'Якуб Пьотровски', '1997-10-04', 'MF', 6, 'Active'),
 (18, 2, 'Ивайло Чочев', '1993-02-18', 'MF', 18, 'Active'),
-(19, 2, 'Филип Калоц', '2000-02-27', 'MF', 8, 'Active'),
-(20, 2, 'Кайо Видал', '2000-11-04', 'FW', 11, 'Active'),
-(21, 2, 'Бърнард Текпетей', '1997-09-03', 'FW', 37, 'Active'),
+(20, 1, 'Кайо Видал', '2000-11-04', 'FW', 11, 'Active'),
 (22, 2, 'Ив Ерик Биле', '2004-12-24', 'FW', 29, 'Active'),
 (23, 3, 'Густаво Бусато', '1990-08-23', 'GK', 1, 'Active'),
 (24, 3, 'Юрген Матей', '1993-04-01', 'DF', 2, 'Active'),
@@ -94,13 +92,13 @@ INSERT INTO `players` (`id`, `club_id`, `full_name`, `birth_date`, `position`, `
 (26, 3, 'Тибо Вион', '1993-12-11', 'DF', 15, 'Active'),
 (27, 3, 'Брадли Де Нойер', '1997-11-13', 'DF', 5, 'Active'),
 (28, 3, 'Амос Юга', '1992-12-08', 'MF', 21, 'Active'),
-(29, 3, 'Йонатан Линдсет', '1996-02-25', 'MF', 7, 'Active'),
+(29, 1, 'Йонатан Линдсет', '1996-02-25', 'MF', 7, 'Active'),
 (30, 3, 'Тобиас Хайнц', '1998-07-13', 'MF', 14, 'Active'),
 (31, 3, 'Маурисио Гарсес', '1997-07-16', 'FW', 10, 'Active'),
 (32, 3, 'Дъкенс Назон', '1994-04-07', 'FW', 9, 'Active'),
-(33, 3, 'Геферсон', '1994-07-13', 'DF', 3, 'Active'),
+(33, 4, 'Геферсон', '1994-07-13', 'DF', 3, 'Active'),
 (34, 4, 'Иван Дюлгеров', '1999-07-15', 'GK', 25, 'Active'),
-(35, 4, 'Васил Панайотов', '1990-07-16', 'MF', 71, 'Active'),
+(35, 4, 'Васил Панайотов', '1990-07-16', 'DF', 71, 'Suspended'),
 (36, 4, 'Живко Атанасов', '1991-02-03', 'DF', 3, 'Active'),
 (37, 4, 'Даниел Димов', '1989-01-21', 'DF', 27, 'Active'),
 (38, 4, 'Цветомир Панов', '1990-08-22', 'DF', 2, 'Active'),
@@ -109,7 +107,35 @@ INSERT INTO `players` (`id`, `club_id`, `full_name`, `birth_date`, `position`, `
 (41, 4, 'Илиан Илиев', '1999-08-07', 'MF', 10, 'Active'),
 (42, 4, 'Дуду', '1997-04-21', 'FW', 11, 'Active'),
 (43, 4, 'Матеус Машадо', '1998-06-15', 'FW', 9, 'Active'),
-(44, 4, 'Атанас Илиев', '1994-07-09', 'FW', 19, 'Active');
+(44, 4, 'Атанас Илиев', '1994-07-09', 'FW', 19, 'Active'),
+(45, 4, 'Тест Тестов', '1995-07-14', 'GK', 67, 'Active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transfers`
+--
+
+CREATE TABLE `transfers` (
+  `TransferId` int(11) NOT NULL,
+  `PlayerId` int(11) NOT NULL,
+  `FromClubId` int(11) DEFAULT NULL,
+  `ToClubId` int(11) NOT NULL,
+  `TransferDate` datetime NOT NULL,
+  `Fee` decimal(15,2) DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transfers`
+--
+
+INSERT INTO `transfers` (`TransferId`, `PlayerId`, `FromClubId`, `ToClubId`, `TransferDate`, `Fee`) VALUES
+(1, 20, NULL, 1, '2026-03-20 11:40:43', 1.00),
+(2, 13, 2, 3, '2026-03-22 11:19:31', 100.00),
+(3, 4, 1, 3, '2026-03-22 11:19:31', 100.00),
+(4, 29, 3, 1, '2026-03-22 11:21:41', 12345678.00),
+(5, 33, 3, 4, '2026-03-22 12:27:25', 123456.21),
+(6, 6, 1, 3, '2026-03-22 13:12:48', 35000.00);
 
 --
 -- Indexes for dumped tables
@@ -130,6 +156,15 @@ ALTER TABLE `players`
   ADD KEY `fk_player_club` (`club_id`);
 
 --
+-- Indexes for table `transfers`
+--
+ALTER TABLE `transfers`
+  ADD PRIMARY KEY (`TransferId`),
+  ADD KEY `PlayerId` (`PlayerId`),
+  ADD KEY `FromClubId` (`FromClubId`),
+  ADD KEY `ToClubId` (`ToClubId`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -143,7 +178,13 @@ ALTER TABLE `clubs`
 -- AUTO_INCREMENT for table `players`
 --
 ALTER TABLE `players`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT for table `transfers`
+--
+ALTER TABLE `transfers`
+  MODIFY `TransferId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -154,6 +195,14 @@ ALTER TABLE `players`
 --
 ALTER TABLE `players`
   ADD CONSTRAINT `fk_player_club` FOREIGN KEY (`club_id`) REFERENCES `clubs` (`id`);
+
+--
+-- Constraints for table `transfers`
+--
+ALTER TABLE `transfers`
+  ADD CONSTRAINT `transfers_ibfk_1` FOREIGN KEY (`PlayerId`) REFERENCES `players` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `transfers_ibfk_2` FOREIGN KEY (`FromClubId`) REFERENCES `clubs` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `transfers_ibfk_3` FOREIGN KEY (`ToClubId`) REFERENCES `clubs` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
